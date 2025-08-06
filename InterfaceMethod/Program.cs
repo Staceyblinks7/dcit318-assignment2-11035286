e6dd4da628d37d81f9723b69f2d776d638e027ca
Anastarcia Duodu <aduodu011@st.ug.edu.gh>
2025-08-06 06:04:23 -0700

    Task 3: InterfaceMethod

---
+++ InterfaceMethod/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace InterfaceMethod
+{
+    // Define interface
+    interface IMovable
+    {
+        void Move();
+    }
+
+    // Implement interface in Car class
+    class Car : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Car is moving");
+        }
+    }
+
+    // Implement interface in Bicycle class
+    class Bicycle : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Bicycle is moving");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create IMovable references to Car and Bicycle
+            IMovable myCar = new Car();
+            IMovable myBicycle = new Bicycle();
+
+            // Call Move() method
+            myCar.Move();       // Output: Car is moving
+            myBicycle.Move();   // Output: Bicycle is moving
+
+            Console.ReadLine(); // Keep console open
+        }
+    }
+}
